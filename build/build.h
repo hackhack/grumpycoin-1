bff2298cd71ba00677be8719b6c4100c2429d512
Carlos <carlos@carlos-VirtualBox.(none)>
2014-01-07 12:32:18 -0600

    changes

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2014-01-02 22:34:42 -0600"
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("GPC");
+    case mBTC: return QString("mGPC");
+    case uBTC: return QString::fromUtf8("μGPC");
     default: return QString("???");
     }
 }
